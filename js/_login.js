da24f1171b0171e365458bb07cedf3fd57e75be8
Ula <ukazmierczyk@gmail.com>
2017-08-11 14:23:33 +0200

    accessibility and API connection added

--- js/_login.js
+++ js/_login.js
@@ -3,9 +3,19 @@ const button = document.querySelectorAll("input")[1];
 const eyeButton = document.querySelectorAll("button")[2];
 
 (function() {
+    input.addEventListener("keyup", function(event) {
+        input.style.border = "none";
+        info.style.visibility = "hidden";
+    });
     button.addEventListener("click", function(event){
         event.preventDefault();
         checkPassword();
+    });
+    input.addEventListener("keypress", function(event){
+        if (event.keyCode === 13) { /* 13 == enter */
+            event.preventDefault();
+            checkPassword();
+        }
     });
     eyeButton.addEventListener("click", function(event) {
        event.preventDefault();
@@ -13,31 +23,47 @@ const eyeButton = document.querySelectorAll("button")[2];
     });
 })();
 
-function checkPassword() {
-    var info = document.querySelector("form .error");
+var info = document.querySelector("form .error");
 
-    if(!input.value) {
-        input.style.border = "solid 2px red";
-        info.style.visibility = "visible";
-        setInterval(function() {
+function checkPassword() {
             if(input.value) {
-                input.style.border = "solid 2px lightgreen";
-                info.style.visibility = "hidden";
-                button.addEventListener("click", function(event) {
-                    window.open('index.html');
-                });
+                checkResponse();
             }
             else {
                 input.style.border = "solid 2px red";
-                info.style.visibility = "visible";
             }
-        }, 500);
-    }
-    else {
-        window.open('index.html');
-    }
+}
+
+function checkResponse() {
+    $.ajax({
+        type: "post",
+        data: {
+            login: "efi",
+            password: input.value
+        },
+        url: "https://efigence-camp.herokuapp.com/api/login",
+        error: function(response) {
+            console.log(response);
+            info.style.visibility = "visible";
+            info.innerText = response.responseJSON.message;
+        },
+        success: function(response) {
+            console.log("succes", response);
+
+            input.style.border = "solid 2px lightgreen";
+            info.style.visibility = "hidden";
+        }
+    });
 }
 
 function showPassword() {
-    input.type === 'password' ? input.type = 'text' : input.type = 'password';
+    if (input.type === 'password') {
+        input.type = 'text';
+        eyeButton.style.opacity = '0.4';
+    }
+    else {
+        input.type = 'password';
+        eyeButton.style.opacity = '1';
+    }
+
 }
